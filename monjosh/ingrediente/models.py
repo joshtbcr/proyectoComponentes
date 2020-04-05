eaaab02c034ae7f5c5cda4936760d8b01b33eff2
Mónica <mzunigaa@ucenfotec.ac.cr>
2020-04-04 22:26:17 -0600

    chau django

--- monjosh/ingrediente/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Ingrediente(models.Model):
-    numeroOrden = models.CharField(max_length=200)
-    fecha = models.DateTimeField('Fecha de registro')
-    cantidad = models.PositiveIntegerField()
