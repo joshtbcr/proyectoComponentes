a243a4e79a1a9f4461a2528546bff1183b20bb3f
Mónica <mzunigaa@ucenfotec.ac.cr>
2020-04-05 16:01:44 -0600

    actualizando librerias de jsonify y autopep y dockerfile

--- monjosh/app.py
+++ monjosh/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template,request,redirect,url_for # For flask implementation
-from bson import ObjectId # For ObjectId to work
+from flask import Flask, render_template, request, redirect, url_for
+from bson import ObjectId
 from pymongo import MongoClient
 import os
 
@@ -7,24 +7,86 @@ app = Flask(__name__)
 title = "Monjosh App"
 heading = "Monjosh App"
 
-##Un-Comment when running against the Cosmos DB Emulator
-client = MongoClient("mongodb://monjoshdb:2PBsBQi43gAKECaFF8A1bYrQeEHxVUrllDN82V8T3gcTrOsPFT6TqeHdqHyNXJbDoYOWorosspRW2pXrT8YVlQ==@monjoshdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@monjoshdb@") #host uri
+# Conexion con API de MongoDB de Cosmos
+client = MongoClient("mongodb://monjoshdb:2PBsBQi43gAKECaFF8A1bYrQeEHxVUrllDN82V8T3gcTrOsPFT6TqeHdqHyNXJbDoYOWorosspRW2pXrT8YVlQ==@monjoshdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@monjoshdb@")  # host uri
 db = client.monjoshdb
-db.authenticate(name="monjoshdb",password='2PBsBQi43gAKECaFF8A1bYrQeEHxVUrllDN82V8T3gcTrOsPFT6TqeHdqHyNXJbDoYOWorosspRW2pXrT8YVlQ==')
+db.authenticate(name="monjoshdb",
+                password='2PBsBQi43gAKECaFF8A1bYrQeEHxVUrllDN82V8T3gcTrOsPFT6TqeHdqHyNXJbDoYOWorosspRW2pXrT8YVlQ==')
 
-collection = db.productos
+# Nombre de colecciones
+productosCollection = db.productos
+ordenesCollection = db.ordenes
+ingredientesCollection = db.ingredientes
+usuariosCollection = db.usuarios
 
+# Metodos RESTful
 @app.route('/')
 def hello():
-    return 'This Compose/Flask demo has been viewed'
-
-@app.route("/action", methods=['POST'])
-def action ():
-	#Adding a Task
-	productoid='1001'
-	collection.insert({ "productoid":productoid, "name":"productoprueba"})
-	return redirect("/")
+    return 'Vista general'
 
 
+@app.route("/ingrediente", methods=['POST'])
+def ingresarIngrediente():
+    nombre = "nombreIngredientePrueba"
+    cantidad = "cantidadIngredientePrueba"
+    ingredientesCollection.insert(
+        {
+            'nombre': nombre,
+            'precio': cantidad
+        }
+    )
+    return redirect("/")
+
+##@app.route("/ingredientes/all", methods=['GET'])
+##def listaIngredientes():
+    ##lstingredientes = ingredientesCollection.find()
+	##return jsonify()
+
+@app.route("/producto", methods=['POST'])
+def ingresarProducto():
+    nombreProducto = "nombreProductoPrueba"
+    precio = "precioProductoPrueba"
+    productosCollection.insert(
+        {
+            'nombre': nombreProducto, 
+            'precio': precio
+        }
+    )
+    return redirect("/")
+
+
+@app.route("/orden", methods=['POST'])
+def ingresarOrden():
+    numeroOrden = "nombreProductoPrueba"
+    fecha = "precioProductoPrueba"
+    estado = "precioProductoPrueba"
+    precioOrden = "precioProductoPrueba"
+    ordenesCollection.insert(
+        {
+            'numeroOrden': numeroOrden, 
+            'fecha': fecha,
+            'estado': estado, 
+            'precio': precioOrden
+        }
+    )
+    return redirect("/")
+
+
+@app.route("/usuario", methods=['POST'])
+def ingresarUsuario():
+    username = "usernamePrueba"
+    password = "passwordPrueba"
+    email = "emailPrueba"
+    usuariosCollection.insert(
+        {
+            "username": username, 
+            "password": password, 
+            "email": email
+        }
+    )
+    return redirect("/")
+
+
+# APP.RUN y puerto
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
