8f4d7902a175a98ac64776ad11000e1d812beaa6
Mónica <mzunigaa@ucenfotec.ac.cr>
2020-04-05 18:35:51 -0600

    Listar ingredientes

--- monjosh/app.py
+++ monjosh/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from bson import ObjectId
 from pymongo import MongoClient
 import os
+import logging
+import json
+from bson import json_util
+##from queue_messages import QueueMessage
+##from azure.storage.common import CloudStorageAccount
 
 app = Flask(__name__)
 title = "Monjosh App"
 heading = "Monjosh App"
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO, filename='example.log')
 
 # Conexion con API de MongoDB de Cosmos
 client = MongoClient("mongodb://monjoshdb:2PBsBQi43gAKECaFF8A1bYrQeEHxVUrllDN82V8T3gcTrOsPFT6TqeHdqHyNXJbDoYOWorosspRW2pXrT8YVlQ==@monjoshdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@monjoshdb@")  # host uri
@@ -37,22 +44,24 @@ def ingresarIngrediente():
     )
     return redirect("/")
 
-##@app.route("/ingredientes/all", methods=['GET'])
-##def listaIngredientes():
-    ##lstingredientes = ingredientesCollection.find()
-	##return jsonify()
 
-@app.route("/producto", methods=['POST'])
-def ingresarProducto():
-    nombreProducto = "nombreProductoPrueba"
-    precio = "precioProductoPrueba"
-    productosCollection.insert(
-        {
-            'nombre': nombreProducto, 
-            'precio': precio
-        }
-    )
-    return redirect("/")
+@app.route("/ingredientes/all", methods=['GET'])
+def listaIngredientes():
+    try:
+        logging.info(f'INICIO ==> OBTENER TODOS INGREDIENTES')
+        
+        ingredientes = ingredientesCollection.find()
+        ingredientes_resultado = []
+        for ingrediente in ingredientes:
+            ingrediente.pop('_id')
+            ingredientes_resultado.append(ingrediente)
+            
+        logging.info(f'FIN ==> OBTENER TODOS INGREDIENTES')
+
+        return jsonify({'ingredientes': ingredientes_resultado})
+    except Exception as exc:
+        logging.error(f'ERROR: No se pueden obtener los ingredientes: {exc}')
+        return None
 
 
 @app.route("/orden", methods=['POST'])
@@ -63,9 +72,9 @@ def ingresarOrden():
     precioOrden = "precioProductoPrueba"
     ordenesCollection.insert(
         {
-            'numeroOrden': numeroOrden, 
+            'numeroOrden': numeroOrden,
             'fecha': fecha,
-            'estado': estado, 
+            'estado': estado,
             'precio': precioOrden
         }
     )
@@ -79,8 +88,8 @@ def ingresarUsuario():
     email = "emailPrueba"
     usuariosCollection.insert(
         {
-            "username": username, 
-            "password": password, 
+            "username": username,
+            "password": password,
             "email": email
         }
     )
